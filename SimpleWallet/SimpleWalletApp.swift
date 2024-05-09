aa8480edbfdeaea65278d076d8e27d56fb8bbad9
Tawan Boonma <tboonma@hotmail.com>
2024-05-09 16:37:12 +0700

    Initial Commit

---
+++ SimpleWallet/SimpleWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleWalletApp.swift
+//  SimpleWallet
+//
+//  Created by Tawan Boonma on 9/5/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct SimpleWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
